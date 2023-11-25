48283f23eab58ce26ecf9c0bf69b6b64830126eb
durianc <143918614+durianc@users.noreply.github.com>
2023-11-25 21:38:16 +0800

    更新后端代码，可以实现与webgl互发消息

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sanic import Sanic
 from sanic.response import file
+
 import json
 
 app = Sanic(__name__)
@@ -16,71 +17,87 @@ rooms = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 def index(request):
     return file('static/index.html')
 
-@app.websocket('/game/<int:room_id>')
-def game(request, ws, room_id):
-    user_id = request.args.get('id')
 
-    if not user_id:
-        return
-
-    if room_id not in rooms:
-        return
-
-    if room_id not in house:
-        house[room_id] = {
-            'players': {},
-            'pieces': [],
-            'sends': [],
-        }
-
-    room = house[room_id]
-    if user_id not in room['players']:
-        room['players'][user_id] = {
-            'ws': ws,
-            'ready': False,
-        }
-
-    ws.send(json.dumps({
-        'type': 'InitializeRoomState',
-        'pieces': room['pieces'],
-        'players': list(room['players'].keys()),
-        'ready': all(player['ready'] for player in room['players'].values()),
-    }))
-
-    for player_id, player_data in room['players'].items():
-        if player_id != user_id:
-            player_data['ws'].send(json.dumps({
-                'type': 'AddPlayer',
-                'ready': all(player['ready'] for player in room['players'].values()),
-            }))
-
+@app.websocket('/game/<room_id>')
+async def feed(request, ws, room_id):
     while True:
-        event = ws.recv()
-        data = json.loads(event)
-        if data['type'] == 'DropPiece':
-            room['pieces'].append((data['x'], data['y']))
-            for player_id, player_data in room['players'].items():
-                if player_id != user_id:
-                    player_data['ws'].send(json.dumps({
-                        'type': 'DropPiece',
-                        'x': data['x'],
-                        'y': data['y'],
-                    }))
-        elif data['type'] == 'SetPlayerReady':
-            player_data = room['players'][user_id]
-            player_data['ready'] = True
-            ws.send(json.dumps({
-                'type': 'PlayerReady',
-                'ready': all(player['ready'] for player in room['players'].values()),
-            }))
+        data = 'Hello from the server!'
+        print('Sending: ' + data)
+        await ws.send(data)
 
-            for player_id, player_data in room['players'].items():
-                if player_id != user_id:
-                    player_data['ws'].send(json.dumps({
-                        'type': 'AddPlayer',
-                        'ready': all(player['ready'] for player in room['players'].values()),
-                    }))
+        # 接收消息
+        data = await ws.recv()
+
+        # 如果数据是bytes类型，则解码为str
+        if isinstance(data, bytes):
+            data = data.decode('utf-8')  # 或者使用你期望的字符集
+
+        print('Received: ' + data)
+# async def game(request, ws, room_id):  # 使用 `async` 定义异步函数
+#     user_id = request.args.get('id')
+#
+#     if not user_id:
+#         return
+#
+#     if room_id not in rooms:
+#         return
+#
+#     if room_id not in house:
+#         house[room_id] = {
+#             'players': {},
+#             'pieces': [],
+#             'sends': [],
+#         }
+#
+#     room = house[room_id]
+#     if user_id not in room['players']:
+#         room['players'][user_id] = {
+#             'ws': ws,
+#             'ready': False,
+#         }
+#     await ws.send('Hello!')
+    # await ws.send(json.dumps({  # 使用 `await` 等待异步操作完成
+    #     'type': 'InitializeRoomState',
+    #     'pieces': room['pieces'],
+    #     'players': list(room['players'].keys()),
+    #     'ready': all(player['ready'] for player in room['players'].values()),
+    # }))
+    #
+    # for player_id, player_data in room['players'].items():
+    #     if player_id != user_id:
+    #         await player_data['ws'].send(json.dumps({  # 使用 `await` 等待异步操作完成
+    #             'type': 'AddPlayer',
+    #             'ready': all(player['ready'] for player in room['players'].values()),
+    #         }))
+    #
+    # while True:
+    #     event = await ws.recv()  # 使用 `await` 等待异步操作完成
+    #     data = json.loads(event)
+    #     if data['type'] == 'DropPiece':
+    #         room['pieces'].append((data['x'], data['y']))
+    #         for player_id, player_data in room['players'].items():
+    #             if player_id != user_id:
+    #                 await player_data['ws'].send(json.dumps({  # 使用 `await` 等待异步操作完成
+    #                     'type': 'DropPiece',
+    #                     'x': data['x'],
+    #                     'y': data['y'],
+    #                 }))
+    #     elif data['type'] == 'SetPlayerReady':
+    #         player_data = room['players'][user_id]
+    #         player_data['ready'] = True
+    #         await ws.send(json.dumps({  # 使用 `await` 等待异步操作完成
+    #             'type': 'PlayerReady',
+    #             'ready': all(player['ready'] for player in room['players'].values()),
+    #         }))
+    #
+    #         for player_id, player_data in room['players'].items():
+    #             if player_id != user_id:
+    #                 await player_data['ws'].send(json.dumps({  # 使用 `await` 等待异步操作完成
+    #                     'type': 'AddPlayer',
+    #                     'ready': all(player['ready'] for player in room['players'].values()),
+    #                 }))
+
 
 
 if __name__ == '__main__':
-    app.run(host='10.133.18.99', port=60001,debug=True)
+    app.run(host='10.133.28.55', port=60001,debug=True)
