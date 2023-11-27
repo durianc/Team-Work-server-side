a2616c5a581d50779131c53cca31e4d70b272a82
durianc <143918614+durianc@users.noreply.github.com>
2023-11-27 09:58:57 +0800

    更新后端，后端可以为新连接分配房间id和玩家id

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from sanic import Sanic
+from sanic import Sanic, response
 from sanic.response import file
-
+from collections import defaultdict
 import json
 
 app = Sanic(__name__)
@@ -9,95 +9,86 @@ with open('./static/index.html', 'rb') as fp:
     html = fp.read()
 
 house = {}
-
-# 增加一个列表用于存储房间号
-rooms = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-
 @app.route('/')
 def index(request):
     return file('static/index.html')
 
+# 一个存储房间和玩家状态的字典
+rooms = defaultdict(lambda: {"players": {}, "game_state": {}})
 
-@app.websocket('/game/<room_id>')
-async def feed(request, ws, room_id):
+
+def generate_room_id():
+    for room_number in range(1, 13):  # 遍历1到12的房间号码
+        room_id = str(room_number)
+        if not is_room_full(room_id):
+            return room_id
+    return None  # 所有房间都满了
+def is_room_full(room_id):
+    return len(rooms[room_id]["players"]) >= 2
+
+def generate_user_id(room_id):
+    # 如果房间是空的，返回 "0"
+    if len(rooms[room_id]["players"]) == 0:
+        return "0"
+    # 如果房间有一个玩家，返回 "1"
+    elif len(rooms[room_id]["players"]) == 1:
+        return "1"
+    # 超过两个玩家则是异常情况
+    else:
+        raise ValueError("Room is already full")
+
+@app.websocket('/game', name='game_without_room_id')
+@app.websocket('/game/<room_id>', name='game_with_room_id')
+async def game(request, ws, room_id=None):
+    # 如果没有提供 room_id，为用户分配一个房间
+    if room_id is None:
+        room_id = generate_room_id()
+        if room_id is None:
+            await ws.send(json.dumps({"type": "Error", "message": "All rooms are full"}))
+            await ws.close(code=1000)
+            return
+
+        # 新房间可能需要在这里初始化
+        if room_id not in rooms:
+            rooms[room_id] = {"players": {}, "game_state": {}}
+
+    try:
+        user_id = generate_user_id(room_id)
+    except ValueError:
+        await ws.send(json.dumps({"type": "Error", "message": "Room is full"}))
+        await ws.close(code=1000)
+        return
+
+        # 如果成功生成 user_id，我们继续加入房间
+    room = rooms[room_id]
+    room["players"][user_id] = ws
+
+    # 向用户发送其ID和房间ID
+    await ws.send(json.dumps({"type": "UserInfo", "room_id": room_id, "user_id": user_id }))
+    # 监听玩家的消息
     while True:
-        data = 'Hello from the server!'
-        print('Sending: ' + data)
-        await ws.send(data)
-
-        # 接收消息
         data = await ws.recv()
-
-        # 如果数据是bytes类型，则解码为str
-        if isinstance(data, bytes):
-            data = data.decode('utf-8')  # 或者使用你期望的字符集
-
-        print('Received: ' + data)
-# async def game(request, ws, room_id):  # 使用 `async` 定义异步函数
-#     user_id = request.args.get('id')
-#
-#     if not user_id:
-#         return
-#
-#     if room_id not in rooms:
-#         return
-#
-#     if room_id not in house:
-#         house[room_id] = {
-#             'players': {},
-#             'pieces': [],
-#             'sends': [],
-#         }
-#
-#     room = house[room_id]
-#     if user_id not in room['players']:
-#         room['players'][user_id] = {
-#             'ws': ws,
-#             'ready': False,
-#         }
-#     await ws.send('Hello!')
-    # await ws.send(json.dumps({  # 使用 `await` 等待异步操作完成
-    #     'type': 'InitializeRoomState',
-    #     'pieces': room['pieces'],
-    #     'players': list(room['players'].keys()),
-    #     'ready': all(player['ready'] for player in room['players'].values()),
-    # }))
-    #
-    # for player_id, player_data in room['players'].items():
-    #     if player_id != user_id:
-    #         await player_data['ws'].send(json.dumps({  # 使用 `await` 等待异步操作完成
-    #             'type': 'AddPlayer',
-    #             'ready': all(player['ready'] for player in room['players'].values()),
-    #         }))
-    #
-    # while True:
-    #     event = await ws.recv()  # 使用 `await` 等待异步操作完成
-    #     data = json.loads(event)
-    #     if data['type'] == 'DropPiece':
-    #         room['pieces'].append((data['x'], data['y']))
-    #         for player_id, player_data in room['players'].items():
-    #             if player_id != user_id:
-    #                 await player_data['ws'].send(json.dumps({  # 使用 `await` 等待异步操作完成
-    #                     'type': 'DropPiece',
-    #                     'x': data['x'],
-    #                     'y': data['y'],
-    #                 }))
-    #     elif data['type'] == 'SetPlayerReady':
-    #         player_data = room['players'][user_id]
-    #         player_data['ready'] = True
-    #         await ws.send(json.dumps({  # 使用 `await` 等待异步操作完成
-    #             'type': 'PlayerReady',
-    #             'ready': all(player['ready'] for player in room['players'].values()),
-    #         }))
-    #
-    #         for player_id, player_data in room['players'].items():
-    #             if player_id != user_id:
-    #                 await player_data['ws'].send(json.dumps({  # 使用 `await` 等待异步操作完成
-    #                     'type': 'AddPlayer',
-    #                     'ready': all(player['ready'] for player in room['players'].values()),
-    #                 }))
+        print(data)
+        # message = json.loads(data)
+        #
+        # # 根据消息类型处理游戏逻辑
+        # if message["type"] == "Action":
+        #     # 更新游戏状态
+        #     pass  # 替换为具体的游戏逻辑代码
+        #
+        # # 广播更新后的游戏状态给房间内的所有玩家
+        # for player_id, player_ws in room["players"].items():
+        #     if player_ws != ws:  # 不要将消息发送回来源玩家
+        #         await player_ws.send(json.dumps({
+        #             "type": "Update",
+        #             "game_state": room["game_state"]
+        #         }))
 
 
-
+# 运行服务端
 if __name__ == '__main__':
-    app.run(host='10.133.28.55', port=60001,debug=True)
+    app.run(host='172.20.10.3', port=60001,debug=True)
+
+# 启用单进程模式后，你可以更容易地查看任何启动错误，因为所有错误都将直接输出到控制台
+# if __name__ == '__main__':
+#     app.run(host='172.20.10.3', port=60001, debug=True, single_process=True)
